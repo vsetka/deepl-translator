7f601b8e43604ea6f5ef99887a413771334b8b41
Vladimir Setka <vsetka@gmail.com>
2017-09-09 22:47:17 +0200

    chore: add an example with input auto detect

--- example.js
+++ example.js
@@ -1,9 +1,16 @@
 const { translate, detectLanguage } = require('./index');
 
+// Translate text with explicit source and target languages
 translate('Die Übersetzungsqualität von deepl ist erstaunlich!', 'EN', 'DE')
   .then(res => console.log(`Translation: ${res.translation}`))
   .catch(console.error);
 
+// Leave out the source language or specify 'auto' to autodetect the input
+translate('Die Übersetzungsqualität von deepl ist erstaunlich!', 'EN')
+  .then(res => console.log(`Translation: ${res.translation}`))
+  .catch(console.error);
+
+// Detect the text language without giving back the translation
 detectLanguage('Deepl también puede detectar un idioma. ¿Qué idioma es este?')
   .then(res => console.log(`Detected language: ${res.languageName}`))
   .catch(console.error);
